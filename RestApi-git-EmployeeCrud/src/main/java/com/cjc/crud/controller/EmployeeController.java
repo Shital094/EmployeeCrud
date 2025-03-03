2d0be79d3edf70f37917dc40c0b4f8495fd7afb9
HP <s.s.potewar@gmail.com>
2025-03-03 17:02:39 +0530

    by shital

--- RestApi-git-EmployeeCrud/src/main/java/com/cjc/crud/controller/EmployeeController.java
+++ RestApi-git-EmployeeCrud/src/main/java/com/cjc/crud/controller/EmployeeController.java
@@ -1,26 +1,24 @@
 package com.cjc.crud.controller;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-
 import org.springframework.web.bind.annotation.DeleteMapping;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
 import org.springframework.web.bind.annotation.RestController;
+
+
 import com.cjc.crud.model.Employee;
 import com.cjc.crud.servicei.EmployeeServiceI;
 @RestController
 public class EmployeeController {
  @Autowired
  EmployeeServiceI esi;
-  @PostMapping("/postdata")
+ 
+
+ @PostMapping("/postdata")
  public String postdata(@RequestBody Employee e) {
 	 esi.postData(e);
 	return"Data Inserted Successfully..";
@@ -41,5 +39,5 @@ public class EmployeeController {
 	 
  }
  
-
+	
 }
